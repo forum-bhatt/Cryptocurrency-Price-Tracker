578c1b9ed368ca20cea6a940b6f2b5008b5f5189
Forum Bhatt <bhatt.f@northeastern.edu>
2023-06-17 01:30:15 -0400

    Header Component and Crypto Marquee

---
+++ src/app/coin-list/coin-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+@Component({
+  selector: 'app-coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrls: ['./coin-list.component.scss']
+})
+export class CoinListComponent implements OnInit {
+
+  bannerData: any = [];
+
+  constructor(private api : ApiService) {}
+
+  ngOnInit() : void {
+    this.getAllData();
+    this.getBannerData();
+  }
+
+  getBannerData(){
+    this.api.getTrendingCurrency("USD")
+    .subscribe(res =>{
+      console.log(res);
+      this.bannerData = res;
+    })
+  }
+
+  getAllData(){
+    this.api.getCurrency("USD")
+    .subscribe(res =>{
+      console.log(res);
+    })
+  }
+
+}
